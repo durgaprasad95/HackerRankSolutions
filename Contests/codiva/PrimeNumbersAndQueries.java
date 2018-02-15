d7c493a7f36655077bb9b33c60bd283a7e381b2b
Durga Prasada Murthy <durgaprasadmurthy@live.com>
2018-02-15 20:07:17 +0530

    Implemented Using SoE for finding primes

--- Contests/codiva/PrimeNumbersAndQueries.java
+++ Contests/codiva/PrimeNumbersAndQueries.java
@@ -6,70 +6,49 @@ import java.io.InputStreamReader;
 import java.util.Arrays;
 
 public class PrimeNumbersAndQueries {
-	static int maxDifference(int startVal, int endVal) {
+
+	static int maxDifference(int startVal, int endVal, int[] primes) {
 		if (startVal == endVal)
 			return 0;
-		int a = 0, b = 0;
-		for (int i = startVal; i <= endVal; i++) {
-			if (primeCheck(unitsDigitCheck(i), i)) {
-				a = i;
-				break;
-			}
-		}
-		if (a == 0)
-			return 0;
-		for (int i = endVal; i >= a; i--) {
-			if (primeCheck(unitsDigitCheck(i), i)) {
-				b = i;
-				break;
-			}
-		}
-		return b - a;
-	}
-
-	static boolean primeCheck(boolean flag, int num) {
-		int factorsCount = 1;
-		boolean result = true;
-		if (num == 1)
-			return false;
-		if (num == 2)
-			return true;
-		for (int i = 3; num != 1 && i <= (int) Math.ceil(Math.sqrt(num)); i = i + 2) {
-			if (i % 5 == 0)
-				continue;
-			if (flag) {
-				if (num % i != 0) {
-					result = true;
-				} else if ((++factorsCount) > 1) {
-					return false;
+		else {
+			int leastPrime = 0, count = 0, largestPrime = 0;
+			for (int i = startVal; i <= endVal; i++)
+				if (primes[i] == 1) {
+					leastPrime = i;
+					break;
 				}
-			} else
-				return false;
+			for (int i = endVal; i >= startVal; i--)
+				if (primes[i] == 1) {
+					largestPrime = i;
+					break;
+				}
+			return largestPrime - leastPrime;
 		}
-		return result;
 	}
 
-	static boolean unitsDigitCheck(int num) {
-		if (num == 1 || num == 4)
-			return false;
-		if (num == 2 || num == 5)
-			return true;
-		Integer[] digits = { 1, 3, 7, 9 };
-		if (Arrays.asList(digits).contains(num % 10))
-			return true;
-		else
-			return false;
+	// Sieve of Eratosthenes
+	static int[] primeNumbers() {
+		int[] primes = new int[1000001];
+		Arrays.fill(primes, 1);
+		for (int i = 2; i <= 1000000; i++) {
+			if (primes[i] != 0)
+				for (int k = 2 * i; k < 1000000; k += i)
+
+					primes[k] = 0;
+		}
+		return primes;
 	}
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int q = Integer.parseInt(br.readLine().trim());
+		int[] primes = primeNumbers();
 		for (int a0 = 0; a0 < q; a0++) {
 			String str = br.readLine();
 			String input[] = str.split(" ");
 			int startVal = Integer.parseInt(input[0]);
 			int endVal = Integer.parseInt(input[1]);
-			int result = maxDifference(startVal, endVal);
+			int result = maxDifference(startVal, endVal, primes);
 			System.out.println(result);
 		}
 	}
