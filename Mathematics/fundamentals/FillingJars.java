68d45d4d6c82543ecac3fd2c80b1c051a85d06cd
Durga Prasada Murthy <durgaprasadmurthy@live.com>
2018-02-26 18:07:06 +0530

    Solution: 102

--- Mathematics/fundamentals/FillingJars.java
+++ Mathematics/fundamentals/FillingJars.java
@@ -6,16 +6,16 @@ public class Solution {
 
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-		int N = in.nextInt();
-		int M = in.nextInt();
+	int N = in.nextInt();
+	int M = in.nextInt();
         BigInteger temp=BigInteger.ZERO;
-		for (int i = 1; i <= M; i++) {
-			int a = in.nextInt();
-			int b = in.nextInt();
-			int k = in.nextInt();
-            temp=temp.add((BigInteger.valueOf(b-a+1)).multiply(BigInteger.valueOf(k)));
-		}
-		System.out.println(Math.round((temp.divide(BigInteger.valueOf(N))).doubleValue()));
-		in.close();
+	for (int i = 1; i <= M; i++) {
+		int a = in.nextInt();
+		int b = in.nextInt();
+		int k = in.nextInt();
+       		temp=temp.add((BigInteger.valueOf(b-a+1)).multiply(BigInteger.valueOf(k)));
+	}
+	System.out.println(Math.round((temp.divide(BigInteger.valueOf(N))).doubleValue()));
+	in.close();
     }
 }
