ae7b4f2cccc6cfa20dbd93cc689ca04912d8b72a
Durga Prasada Murthy <durgaprasadmurthy@live.com>
2018-06-13 18:14:16 +0530

    Solution: 133

---
+++ Java/strings/JavaRegex.java
@@ -0,0 +1,20 @@
+package core;
+
+import java.util.Scanner;
+
+public class JavaRegex {
+
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		while (in.hasNext()) {
+			String IP = in.next();
+			System.out.println(IP.matches(new MyRegex().pattern));
+		}
+	}
+
+}
+
+class MyRegex {
+
+	String pattern = "(([0-1]?[0-9]{1,2})?(2?[0-5]{1,2})?)[.](([0-1]?[0-9]{1,2})?(2?[0-5]{1,2})?)[.](([0-1]?[0-9]{1,2})?(2?[0-5]{1,2})?)[.](([0-1]?[0-9]{1,2})?(2?[0-5]{1,2})?)";
+}
