08ea7fb3ebebefa09a65e6f9aa31b11bd1d0ceff
Durga Prasada Murthy <durgaprasadmurthy@live.com>
2018-06-13 15:28:38 +0530

    Solution: 132

---
+++ Java/strings/JavaCurrencyFormatter.java
@@ -0,0 +1,20 @@
+package core;
+
+import java.text.NumberFormat;
+import java.util.Locale;
+import java.util.Scanner;
+
+public class JavaCurrencyFormatter {
+
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		Double amount = in.nextDouble();
+		in.close();
+		System.out.println("US: " + NumberFormat.getCurrencyInstance(Locale.US).format(amount));
+		System.out.println("India: " + NumberFormat.getCurrencyInstance(new Locale("en", "IN")).format(amount));
+		System.out.println("China: " + NumberFormat.getCurrencyInstance(Locale.CHINA).format(amount));
+		System.out.println("France: " + NumberFormat.getCurrencyInstance(Locale.FRANCE).format(amount));
+
+	}
+
+}
