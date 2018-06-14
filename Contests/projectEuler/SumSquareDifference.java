32541a1929be2dae35c0c00e29cd1a2ac7c42441
Durga Prasada Murthy <durgaprasadmurthy@live.com>
2018-06-14 23:51:12 +0530

    Solution: 95 [Update]

--- Contests/projectEuler/SumSquareDifference.java
+++ Contests/projectEuler/SumSquareDifference.java
@@ -1,5 +1,6 @@
 package projectEuler;
 
+import java.math.BigInteger;
 import java.util.Scanner;
 
 public class SumSquareDifference {
@@ -7,9 +8,14 @@ public class SumSquareDifference {
 	public static void main(String[] args) {
 		Scanner in = new Scanner(System.in);
 		int t = in.nextInt();
+		BigInteger four = BigInteger.valueOf(4l);
+		BigInteger two = BigInteger.valueOf(2l);
+		BigInteger one = BigInteger.valueOf(1l);
+		BigInteger six = BigInteger.valueOf(6l);
 		for (int a0 = 0; a0 < t; a0++) {
-			int n = in.nextInt();
-			long sum = ((n * n * (n + 1) * (n + 1)) / 4) - ((n * (n + 1) * ((2 * n) + 1)) / 6);
+			BigInteger n = in.nextBigInteger();
+			BigInteger sum = n.multiply(n.add(one)).divide(two).pow(2)
+					.subtract(n.multiply(n.add(one)).multiply(two.multiply(n).add(one)).divide(six));
 			System.out.println(sum);
 		}
 		in.close();
